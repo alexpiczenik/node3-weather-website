caa64067b72c29a6d065cb02ed05b5556dc3e2db
Alex Piczenik <alexpiczenik@Alexs-Air.localdomain>
2021-08-20 12:46:00 -0300

    Agrego detalles

--- src/app.js
+++ src/app.js
@@ -93,6 +93,6 @@ app.get('*', (req, res) => {
         error: 'Page not found'
     })
 })
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
 })
